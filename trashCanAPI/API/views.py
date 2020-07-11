7e4130ec9ffd975337ce086da92930a40d548cd2
bullet46 <1224236193@qq.com>
2020-07-12 00:04:21 +0800

    项目初版 70%完成度提交

--- trashCanAPI/API/views.py
+++ trashCanAPI/API/views.py
@@ -7,9 +7,12 @@ from urllib.parse import unquote
 from API.search import core_search
 
 def index(request):
-    trash = core_search.TrashCanShow()
-    info = str(request).split("/?")
-    info = str(info[1]).split("'>")[0]
-    message = unquote(info,encoding='utf-8')
-    print(message)
-    return HttpResponse(str(trash.trash_message(message)))
+    try:
+        info = str(request).split("/?")
+        info = str(info[1]).split("'>")[0]
+        message = unquote(info,encoding='utf-8')
+        print(message)
+        trash = core_search.TrashCanShow()
+        return HttpResponse(str(trash.trash_message(message)))
+    except:
+        return HttpResponse('None')
