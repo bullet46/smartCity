a7dff5eb8853be90814e994399d27a8cbc79ff15
bullet46 <1224236193@qq.com>
2020-07-21 13:09:07 +0800

    在地图界面中加入了图标

---
+++ SmartCityWeb/static/js/trash_can_info.js
@@ -0,0 +1,22 @@
+var trash_can_info = [
+    {
+        name:'device_0',
+        position:[113.928926,22.571839],
+        icon: '/static/icons/trash_80.png'
+    },
+    {
+        name :'device_1',
+        position: [113.930705,22.570304],
+        icon: 'static/icons/trash_80.png'
+    },
+        {
+        name :'device_2',
+        position: [113.926799,22.570579],
+        icon: 'static/icons/info_offline.png'
+    }
+    ,    {
+        name :'device_3',
+        position: [113.925487,22.572167],
+        icon: 'static/icons/info_fire.png'
+    }
+]
